c9daa8390d9e3a6784aca063caf8431115fb4afa
Sergey <21218903+xchakiex@users.noreply.github.com>
2021-10-30 12:59:05 +0300

    Initial Commit

---
+++ ColorSliderSwiftUI/ColorSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderSwiftUIApp.swift
+//  ColorSliderSwiftUI
+//
+//  Created by Nechaev Sergey  on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
